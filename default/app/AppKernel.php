b25219a74d6295fd79ed7c18969c4d0ae3171254
Manuel Aguirre <programador.manuel@gmail.com>
2012-12-03 10:54:05 -0430

    Actualizando el AppKernel, para obtener la instancia del loader.

--- default/app/AppKernel.php
+++ default/app/AppKernel.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+/* @var $loader Composer\Autoload\ClassLoader */
+$loader = require_once __DIR__ . '/../../vendor/autoload.php';
 
 use KumbiaPHP\Kernel\Kernel;
 
@@ -29,9 +30,19 @@ class AppKernel extends Kernel
 
     protected function registerRoutes()
     {
-        return array(
+        $routes = array(
             '/' => 'Index',
         );
+
+        if (!$this->production) {
+            //$routes['/admin'] = 'Demos/Seguridad';
+            //rutas disponibles solo en desarrollo
+        }
+
+        return $routes;
     }
 
 }
+
+//acá podemos incluir rutas y prefijos al autoloader
+//$loader->add('PHPExcel', __DIR__ . '../../vendor/');
