2999e2fe0ec5644f53d4d9dd461d88e52b5cff98
Alberto Patiño <arp2312@gmail.com>
2012-12-06 14:16:10 -0430

    qqw

---
+++ default/app/modules/Index/Controller/noticiasController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: apatino
+ * Date: 12/6/12
+ * Time: 11:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+//use KumbiaPHP\
+class noticias extends Controller
+{
+
+}
---
+++ default/app/modules/Index/Model/noticias.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: apatino
+ * Date: 12/6/12
+ * Time: 11:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class noticias
+{
+
+}
