795eb7c73d08fcaa23e04de169c5e967805db001
Manuel Aguirre <programador.manuel@gmail.com>
2012-12-06 10:28:53 -0430

    creando modulo para cambio de foto

--- default/app/modules/Index/Controller/indexController.php
+++ default/app/modules/Index/Controller/indexController.php
@@ -2,7 +2,9 @@
 
 namespace Index\Controller;
 
+use KumbiaPHP\Form\Form;
 use Index\Model\Usuarios;
+use KumbiaPHP\Upload\Upload;
 use Index\Model\EquiposRegistrados;
 use KumbiaPHP\Kernel\Controller\Controller;
 
@@ -16,9 +18,60 @@ class indexController extends Controller
 
     public function inicio_action()
     {
-        if ( null === $this->get('security')->getToken('clave') ){
+        if (null === $this->get('security')->getToken('clave')) {
             $this->get('flash')->warning("Debes Crear Tu Contraseña...!!!");
             return $this->getRouter()->forward('registro/cambiar_clave');
         }
     }
+
+    public function foto_action()
+    {
+        $personajes = Usuarios::findAll('array');
+
+        $form = new Form('cambio_foto');
+
+        $form->add('personaje', 'select')
+                ->setOptionsFromResultset($personajes, 'id', 'personaje')
+                ->setDefault('- Seleccionar -')
+                ->setLabel('Personaje')
+                ->required();
+
+        $form->add('imagen', 'file')
+                ->setLabel('Nueva Imagen')
+                ->required();
+
+        if ($this->getRequest()->isMethod('post')) {
+            if ($form->bindRequest($this->getRequest())->isValid()) {
+                $file = $form->getField('imagen')->getValue();
+                //cambiar esto para usar la lib Upload y poder validar el tamaño
+                //y extension de la imagen.
+                $upload = Upload::factory($this->getRequest(), array('cambio_foto', 'imagen'), 'image');
+
+                $path = dirname($this->container['app_dir']) .'public/img/perfiles2/';
+                return;
+
+                $upload->setMinWidth(150);
+                $upload->setMaxWidth(150);
+                $upload->setMinHeight(180);
+                $upload->setMaxHeight(180);
+                $upload->allowOverwrite(true);
+                $upload->setPath($path);
+
+                if ($upload->save()) {
+                    $personaje = Usuarios::findByPK($form['personaje']->getValue());
+                    $personaje->imagen = $path . $upload->getFile()->getName();
+                    if($personaje->save()){
+                        $this->get('flash')->success("Imagen subida con exito");                        
+                    }else{
+                        $this->get('flash')->error($personaje->getErrors());
+                    }
+                }else{
+                    $this->get('flash')->error($upload->getErrors());
+                }
+            }
+        }
+
+        $this->form = $form;
+    }
+
 }
